1cc8cc7ceeb76aaea211c08f89ddf6d49032115f
Kasie <kchaplick92@gmail.com>
2019-03-10 19:59:16 -0400

    css for start screen and js logic to hide

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-//
+$( document ).ready(function() {
+    console.log("ready");
+    
+//Press enter to play function
+$(document).keypress(function(e) {
+    var keycode = (e.keyCode ? e.keyCode : e.which);
+    if (keycode == '13') {
+        console.log('You pressed enter! - keypress');
+        $(".startDiv").hide();
+    }
+});
+
+
+
+
+
+
+
+
+//end docu.ready    
+});
